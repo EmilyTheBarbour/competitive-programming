dd0eba38ee77c9cf687f2480ca8293e6efeac757
Emily Barbour <emilythebarbour@gmail.com>
2018-04-19 14:16:44 -0400

    projectEuler Practice

--- projectEuler/13/pe13.py
+++ projectEuler/13/pe13.py
@@ -1,14 +1,24 @@
 def inputNums():
     arr = []
-    file = open("input.txt")
+    file = open("projectEuler\\13\\input.txt")
 
     for i in range(100):
-        arr.append(int(file.readline))
+        arr.append(file.readline())
+        arr[i] = arr[i][:15]
 
     return arr
 
+def addNums(arr):
+    total = 0
+    for num in arr:
+        total += int(num)
+
+    return str(total)[:10]
+
 def main():
     arr = inputNums()
+    total = addNums(arr)
+    print(total)
 
 if __name__ == "__main__":
     main()
